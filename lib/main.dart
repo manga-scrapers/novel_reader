be22a8812ad79bacb295e9bbab92e14e29f9b52d
p2kr <34896515+p2kr@users.noreply.github.com>
2021-10-24 03:20:19 +0530

    Added panning to Html
    Signed-off-by: p2kr <34896515+p2kr@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:novel_reader/constants.dart';
@@ -12,9 +9,6 @@ import 'package:url_strategy/url_strategy.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
-  if (Platform.isAndroid) {
-    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
-  }
   runApp(const MyApp());
 }
 
